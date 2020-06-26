448b69e550d58e2e73e4442f1daf14b59a313517
Zabana <karim.cheurfi@gmail.com>
2020-06-26 17:56:08 +0200

    increased default gaps, bar padding and opacity

--- config.h
+++ config.h
@@ -10,10 +10,12 @@ static const unsigned int gappov    = 25;       /* vert outer gap between window
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "pango:Inconsolata:size=10:antialias=true", "FontAwesome:size=8:antialias=true:autohint=true" };
-static const char dmenufont[]       = "pango:Inconsolata:size=10:antialias=true";
-static const int horizpadbar        = 2;        /* horizontal padding for statusbar */
-static const int vertpadbar         = 4;        /* vertical padding for statusbar */
+static const char *fonts[]          = {
+    "Liberation Mono:pixelsize=12:antialias=true",
+    "FontAwesome:size=8:antialias=true:autohint=true"
+};
+static const int horizpadbar        = 4;        /* horizontal padding for statusbar */
+static const int vertpadbar         = 10;        /* vertical padding for statusbar */;
 
 /* static const char col_gray1[]       = "#1A202C"; */
 /* static const char col_gray2[]       = "#4A5568"; */
@@ -81,7 +83,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-hp", "chromium", NULL };
 
 static const char *termcmd[]  = { "alacritty", NULL };
 
