518519cdd33ec357a12b912e7f2905c2695afa1e
Zabana <karim.cheurfi@gmail.com>
2020-06-26 15:30:25 +0200

    increased default gaps, bar padding and opacityc

--- config.h
+++ config.h
@@ -3,17 +3,17 @@
 /* appearance */
 static unsigned int borderpx  = 2;        /* border pixel of windows */
 static unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappih    = 25;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 25;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 25;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 25;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "pango:Inconsolata:size=10:antialias=true", "FontAwesome:size=8:antialias=true:autohint=true" };
 static const char dmenufont[]       = "pango:Inconsolata:size=10:antialias=true";
 static const int horizpadbar        = 2;        /* horizontal padding for statusbar */
-static const int vertpadbar         = 2;        /* vertical padding for statusbar */
+static const int vertpadbar         = 4;        /* vertical padding for statusbar */
 
 /* static const char col_gray1[]       = "#1A202C"; */
 /* static const char col_gray2[]       = "#4A5568"; */
@@ -28,7 +28,7 @@ static char selfgcolor[]            = "#eeeeee";
 static char selbordercolor[]        = "#005577";
 static char selbgcolor[]            = "#005577";
 
-static const unsigned int baralpha = 80;
+static const unsigned int baralpha = 100;
 static const unsigned int borderalpha = OPAQUE;
 
 static const char *colors[][3] = {
